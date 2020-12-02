316d91fbf4f258d85810cc0c8b06d0136e051c23
Nikolay Tchakarov <nikitcha@yahoo.com>
2020-12-02 20:08:14 +0100

    added longer runs for 3-4 SAC agents

--- th_rl/run_scenarios.py
+++ th_rl/run_scenarios.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import os
+import click
 
-scenarios = pd.read_excel(r'C:\Users\niki\Source\th_rl\th_rl\scenarios.xlsx')
+@click.command()
+@click.option('--scenariofile', default=r'C:\Users\niki\Source\th_rl\th_rl\scenarios.csv', help='Location of the scenario file', type=str)
+@click.option('--start', default=0, help='Start index', type=int)
+def run(**params):
+    globals().update(params)
+    scenarios = pd.read_csv(scenariofile)
+    for i in range(scenarios.shape[0]):
+        scen = scenarios.iloc[i,:]
+        command = 'python -m th_rl.main --id='+str(i)
+        for s in scen.index:
+            opt = ' --'+s+'='+str(scen[s])
+            command += opt
 
-for i in range(scenarios.shape[0]):
-    scen = scenarios.iloc[i,:]
-    command = 'python -m th_rl.main --id='+str(i)
-    for s in scen.index:
-        opt = ' --'+s+'='+str(scen[s])
-        command += opt
+        if i>=start:
+            print(command)
+            os.system(command)
 
-    print(command)
-    os.system(command)
+if __name__=='__main__':
+    run()
