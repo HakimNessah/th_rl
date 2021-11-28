07b00130ddcef432a340260f58604acd57decefb
Nikolay Tchakarov <nikitcha@yahoo.com>
2021-11-28 15:26:48 +0100

    update

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(name='th_rl',
       license='MIT',
       packages=['th_rl'],
       install_requires=[
-        "click==7.1.2",
-        "numpy==1.19.4",
-        "pandas==1.3.4",
-        "plotly==5.4.0",
-        "torch==1.10.0"
+        "click",
+        "numpy",
+        "pandas",
+        "plotly",
+        "torch"
       ],      
       zip_safe=False)
